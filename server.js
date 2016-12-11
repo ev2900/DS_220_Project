ff9a7f5a02bd05b36e6759aaad8a0e53eab0afdc
Chris Sharkey <ev2900@gmail.com>
2016-12-10 20:04:41 -0500

    update

--- server.js
+++ server.js
@@ -27,15 +27,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Routess
 	//EJS Routes 
 	app.get('/', function(req, res) {
-		res.render("list_your_spot.ejs");
+		res.render("order_pizza.ejs");
 	});
 
 	app.get('/list_your_spot', function(req, res) {
 		res.render("list_your_spot.ejs");
-	});
-
-	app.get('/login', function(req, res) {
-		res.render("login.ejs");
 	});
 
 	app.get('/summer_parking_spaces', function(req, res) {
@@ -48,22 +44,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 		res.render("thank_you_list.ejs", {email_email: email_email, id_id: id_id});
 	});
 
-	app.get('/thank_you_pay', function(req, res) {
-		res.render("thank_you_pay.ejs");
-	});
-
-	app.get('/how_it_works', function(req, res) {
-		res.render("how_it_works.ejs");
-	});
-
-	app.get('/jobs', function(req, res) {
-		res.render("jobs.ejs");
-	});
-
-	app.get('/detail/:id', function(req, res) {
-		var id_url = req.params.id;
-		res.render("details.ejs", {id_url: id_url});
-	});
 //End EJS Routes - sorry really have to put this into a seperate file at some point ...
 
 	app.use('/api', require('./routes/api'));
