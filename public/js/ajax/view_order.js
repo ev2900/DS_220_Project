9a330b8c33cd89d7f581a1846b15b2f63ccdff5a
Chris Sharkey <ev2900@gmail.com>
2016-12-10 20:52:21 -0500

    update

--- public/js/ajax/view_order.js
+++ public/js/ajax/view_order.js
@@ -8,28 +8,21 @@ $(function (){
         success: function(spotz) {
             $.each(spotz, function(i, spot) {
                 $spotz.append( '<div class="row parking-listings">' +
-                                    '<div class="col-lg-4 col-xs-12 listing-title">' +
+                                    '<div class="col-lg-2 col-xs-12 listing-title">' +
+                                        '<h4>' + spot.firstName + spot.lastName + '</h4>' + 
+                                    '</div>'+
+                                    '<div class="col-lg-2 col-xs-12 listing-title">' +
+                                        '<h4>' + spot.type + '</h4>' + 
+                                    '</div>'+
+                                    '<div class="col-lg-2 col-xs-12 listing-title">' +
+                                        '<h4>' + spot.adress + '</h4>' + 
+                                    '</div>'+
+                                    '<div class="col-lg-2 col-xs-12 listing-title">' +
+                                        '<h4>' + spot.phoneNumber + '</h4>' + 
+                                    '</div>'+
+                                    '<div class="col-lg-2 col-xs-12 listing-title">' +
                                         '<h4>' + spot.title + '</h4>' + 
                                     '</div>'+
-                                    '<div class="col-lg-5 col-xs-8 info-block">' +
-                                        '<div class="col-lg-4 col-xs-12 listing-info">' 
-                                            + '<h5>' + spot.type + '</h5>' +
-                                        '</div>' +
-                                        '<div class="col-lg-5 col-xs-12 listing-info">' 
-                                            + '<h5>' + spot.adress + '</h5>' +
-                                        '</div>' +
-                                        '<div class="col-lg-2 col-xs-12 listing-info">' 
-                                            + '<h5>' + "$"+ spot.rate + '</h5>' +
-                                        '</div>' +
-                                    '</div>' +
-                                    '<div class="col-lg-2 col-xs-4">' +
-                                        '<div class="col-lg-6 col-xs-12 ">' +
-                                            '<a href="http://52.37.179.213:9000/detail/' + spot._id + '" class="btn">Details</a>' +
-                                        '</div>' +
-                                        '<div class="col-lg-6 col-xs-12">' +
-                                            '<a href="http://52.37.179.213:9000/payment/' + spot._id + '" class="btn">Book</a>' +
-                                        '</div>' +
-                                    '</div>' +
                                 '</div>' );
             });
         },
