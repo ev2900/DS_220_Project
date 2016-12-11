55cbcf2fb20f025c5253b4365fd9ae747fcc9211
Chris Sharkey <ev2900@gmail.com>
2016-12-10 21:30:07 -0500

    update

--- public/js/ajax/view_order.js
+++ public/js/ajax/view_order.js
@@ -23,9 +23,6 @@ $(function (){
                                     '<div class="col-lg-2 col-xs-12 listing-title">' +
                                         '<h4>' + spot.title + '</h4>' + 
                                     '</div>'+
-                                    '<div class="col-lg-2 col-xs-12 listing-title">' +
-                                         '<form id="submit-spot"> <input type="submit" class="navbar-btn nav-button login btn" value="Submit"> </form>' +
-                                    '</div>'+
                                 '</div>' );
             });
         },
